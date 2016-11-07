dbce421e49a543750a2243e2ad1bc4826b388de2
Evan Cooper <evan.cooper@rogers.com>
2016-11-07 13:55:07 -0500

    Update and rename Task_ScheduleCreator.java to TaskScheduleGenerator.java

--- android/java/TaskScheduleGenerator.java
+++ android/java/TaskScheduleGenerator.java
@@ -1,16 +1,24 @@
-package com.example.coop.schedula;
+package com.example.coop.schedulaui;
 
 import android.os.AsyncTask;
+
+import java.util.ArrayList;
+
+import SchedulaAlgo.Commitment;
+import SchedulaAlgo.Schedule;
 
 /**
  * Created by coop on 2016-10-14.
  */
 
-public class ScheduleCreatorTask extends AsyncTask {
+public class TaskScheduleGenerator extends AsyncTask<Object, Void, Void> {
+
+    private Schedule schedule;
 
     @Override
-    protected Object doInBackground(Object[] params) {
+    protected Void doInBackground(Object[] params) {
 
+        States.SCHEDULE = new Schedule(new ArrayList<Commitment>(), States.SELECTED_COURSES);
 
         return null;
     }
