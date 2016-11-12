eef9d139e0a5cb3be3407a65f47b6d76be4f82cb
Daniel Fitzhenry <fitzhenry8@hotmail.com>
2016-11-12 12:58:38 -0500

    Update Section.java

--- SchedulaAlgo/Section.java
+++ SchedulaAlgo/Section.java
@@ -26,61 +26,61 @@ public class Section {
 	 *			s = array list of SubSections
 	 **/
 	public Section(String n, String p, int c, int term, String time, ArrayList<SubSection> s){
-	int startHour, startMinute, endHour, endMinute;
-	int year = (int)(term/100);
-	int semester = term - year*100;
-	ID = n;
-	prof = p;
-	crn = c;
-	subSec = s;
-	times = new TimeSlot[3];
-	if (time.length() == 11){
-	// If once a week (W 1405-1555) 
-		startHour = Integer.parseInt(time.substring(2,4));
-		startMinute = Integer.parseInt(time.substring(4,6));
-		endHour = Integer.parseInt(time.substring(7,9));
-		endMinute = Integer.parseInt(time.substring(9));
-		times[0] = new TimeSlot(time.charAt(0),startHour,startMinute,endHour,endMinute,semester,year);
-		times[1] = times[2] = null;
-	} else if (time.length() == 12){
-	// If twice a week (TR 1405-1555)
-		startHour = Integer.parseInt(time.substring(3,5));
-		startMinute = Integer.parseInt(time.substring(5,7));
-		endHour = Integer.parseInt(time.substring(8,10));
-		endMinute = Integer.parseInt(time.substring(10));
-		times[0] = new TimeSlot(time.charAt(0),startHour,startMinute,endHour,endMinute,semester,year);
-		times[1] = new TimeSlot(time.charAt(1),startHour,startMinute,endHour,endMinute,semester,year);
-		times[2] = null;
-	} else if (time.length() == 13){
-	// If three times a wekk (MWF 1405-1555)
-		startHour = Integer.parseInt(time.substring(4,6));
-		startMinute = Integer.parseInt(time.substring(6,8));
-		endHour = Integer.parseInt(time.substring(9,11));
-		endMinute = Integer.parseInt(time.substring(11));
-		times[0] = new TimeSlot(time.charAt(0),startHour,startMinute,endHour,endMinute,semester,year);
-		times[1] = new TimeSlot(time.charAt(1),startHour,startMinute,endHour,endMinute,semester,year);
-		times[2] = new TimeSlot(time.charAt(2),startHour,startMinute,endHour,endMinute,semester,year);
-	} else times[0] = times[1] = times[2] = null;
-    }
+		int startHour, startMinute, endHour, endMinute;
+		int year = (int)(term/100);
+		int semester = term - year*100;
+		ID = n;
+		prof = p;
+		crn = c;
+		subSec = s;
+		times = new TimeSlot[3];
+		if (time.length() == 11){
+		// If once a week (W 1405-1555) 
+			startHour = Integer.parseInt(time.substring(2,4));
+			startMinute = Integer.parseInt(time.substring(4,6));
+			endHour = Integer.parseInt(time.substring(7,9));
+			endMinute = Integer.parseInt(time.substring(9));
+			times[0] = new TimeSlot(time.charAt(0),startHour,startMinute,endHour,endMinute,semester,year);
+			times[1] = times[2] = null;
+		} else if (time.length() == 12){
+		// If twice a week (TR 1405-1555)
+			startHour = Integer.parseInt(time.substring(3,5));
+			startMinute = Integer.parseInt(time.substring(5,7));
+			endHour = Integer.parseInt(time.substring(8,10));
+			endMinute = Integer.parseInt(time.substring(10));
+			times[0] = new TimeSlot(time.charAt(0),startHour,startMinute,endHour,endMinute,semester,year);
+			times[1] = new TimeSlot(time.charAt(1),startHour,startMinute,endHour,endMinute,semester,year);
+			times[2] = null;
+		} else if (time.length() == 13){
+		// If three times a week (MWF 1405-1555)
+			startHour = Integer.parseInt(time.substring(4,6));
+			startMinute = Integer.parseInt(time.substring(6,8));
+			endHour = Integer.parseInt(time.substring(9,11));
+			endMinute = Integer.parseInt(time.substring(11));
+			times[0] = new TimeSlot(time.charAt(0),startHour,startMinute,endHour,endMinute,semester,year);
+			times[1] = new TimeSlot(time.charAt(1),startHour,startMinute,endHour,endMinute,semester,year);
+			times[2] = new TimeSlot(time.charAt(2),startHour,startMinute,endHour,endMinute,semester,year);
+		} else times[0] = times[1] = times[2] = null;
+	}
 
 	/** TO STRING
 	 *	Function:	string formatted representation of this Section obj
 	 *	@params	n/a
 	 *	
-	**/
-    public String toString(){
-    	/*String s = "Section: "+ID+"\nProf:"+prof+"\n";
-        for(TimeSlot t : times) if (t != null) s += t+"\n";
-        s += "\nAdditional Requirements: \n"+subSec;
-        return s;*/
+	 **/
+	public String toString(){
+		/*String s = "Section: "+ID+"\nProf:"+prof+"\n";
+		for(TimeSlot t : times) if (t != null) s += t+"\n";
+		s += "\nAdditional Requirements: \n"+subSec;
+		return s;*/
 		return prof+crn;
-    }
+	}
 	
-    /** CONFLICTS
+	/** CONFLICTS
 	 *	Function:	compares two sections to see if their times conflict
 	 *	@params	s = Section that will be compared with this
 	 *	
-    **/
+	 **/
 	public boolean conflicts(Section s) {
 		boolean b = false;
 		if (this == null || s == null) return b;
@@ -98,7 +98,7 @@ public class Section {
 	 *	Function:	adds SubSection to array list of SubSections
 	 *	@params	subSection: SubSection to be added
 	 *
-	**/
+	 **/
 	public void addSubSection(SubSection subSection) { subSec.add(subSection); }
 
    	 // Getters
