925cafb71b50769935120dbab85b12d5c309dde2
Evan Cooper <evan.cooper@rogers.com>
2016-11-13 19:50:21 -0500

    Update States.java

--- android/java/States.java
+++ android/java/States.java
@@ -1,8 +1,11 @@
-package com.example.coop.schedula;
-
-import com.alamkanak.weekview.WeekViewEvent;
+package com.example.coop.schedulaui;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import SchedulaAlgo.Course;
+import SchedulaAlgo.Schedule;
 
 /**
  * Created by coop on 2016-10-13.
@@ -10,11 +13,25 @@ import java.util.ArrayList;
 
 public class States {
 
-    // CourseSelector
-    static Boolean IS_SET = false;
-    static ArrayList<String> SELECTED_COURSES = new ArrayList<>();
-    static ArrayList<String> AVAILABLE_COURSES = new ArrayList<>();
+    // DB
+    static Boolean dbQueried = false;
 
-    // Bundle : CourseSelector -> CoursesFinal
-    static String eventSets;
+    // Course objects
+    static ArrayList<Course> coursesFall = new ArrayList<>();
+    static ArrayList<Course> coursesWint = new ArrayList<>();
+
+    // ActivitySelector
+    static Boolean IS_SET = false;
+    static ArrayList<Course> SELECTED_COURSES = new ArrayList<>();
+    static ArrayList<Course> AVAILABLE_COURSES = new ArrayList<>();
+    static String term = "201630";
+
+    // ActivityPreferences
+    static List<String> specificTimes = new ArrayList<>();
+    static String preferredTOD = "";
+    static ArrayList<String> specificDays = new ArrayList<>();
+    static Boolean PREF_SET = false;
+
+    // ActivityFinal
+    static LinkedList<Schedule> SCHEDULES;
 }
