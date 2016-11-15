bb24c2219ca5f79fd8b859a3dde1051c4b71c887
Evan Cooper <evan.cooper@rogers.com>
2016-11-15 01:53:17 -0500

    Update States.java

--- android/java/States.java
+++ android/java/States.java
@@ -20,13 +20,21 @@ public class States {
     static ArrayList<Course> coursesFall = new ArrayList<>();
     static ArrayList<Course> coursesWint = new ArrayList<>();
 
-    // ActivitySelector
+    // ActivitySelect
     static Boolean IS_SET = false;
-    static ArrayList<Course> SELECTED_COURSES = new ArrayList<>();
-    static ArrayList<Course> AVAILABLE_COURSES = new ArrayList<>();
+    static ArrayList<Course> SELECTED_COURSES_FALL = new ArrayList<>();
+    static ArrayList<Course> SELECTED_COURSES_WINT = new ArrayList<>();
     static String term = "201630";
 
     // ActivityPreferences
+    static List<String> SPECIFIC_TIMES_FALL = new ArrayList<>();
+    static List<String> SPECIFIC_TIMES_WINT = new ArrayList<>();
+    static String PREFERRED_TOD_FALL = "";
+    static String PREFERRED_TOD_WINT = "";
+    static ArrayList<String> SPECIFIC_DAYS_FALL = new ArrayList<>();
+    static ArrayList<String> SPECIFIC_DAYS_WINT = new ArrayList<>();
+
+
     static List<String> specificTimes = new ArrayList<>();
     static String preferredTOD = "";
     static ArrayList<String> specificDays = new ArrayList<>();
