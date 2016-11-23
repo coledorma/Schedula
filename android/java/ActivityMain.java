a3891dfbee2b3a0bc267a46362d87541db76df83
Cole Dorma <coledorma@hotmail.com>
2016-11-23 09:55:36 -0500

    Update ActivityMain.java

--- android/java/ActivityMain.java
+++ android/java/ActivityMain.java
@@ -1,7 +1,11 @@
 package com.example.coop.schedulaui;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -38,10 +42,29 @@ public class ActivityMain extends AppCompatActivity {
             States.dbQueried = true;
         }
 
-        findViewById(R.id.start_button).setOnClickListener(new View.OnClickListener() {
+
+        FloatingActionButton startFAB = (FloatingActionButton) findViewById(R.id.startFAB);
+        startFAB.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 startActivity(new Intent(ActivityMain.this, ActivitySelect.class));
+            }
+        });
+
+        FloatingActionButton infoFAB = (FloatingActionButton) findViewById(R.id.infoFAB);
+        infoFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(ActivityMain.this)
+                        .setTitle("Information")
+                        .setMessage("Authors:\nCole Dorma\nEvan Cooper\nJacob Perks\nDaniel Fitzhenry\n")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .show();
+
             }
         });
     }
